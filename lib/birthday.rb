51a5735877b85f06713272fa75a9cd090c974484
Won Kim <wonsae.kim@gmail.com>
2019-04-15 03:11:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |k, v|
+    if v <= 12
+      puts "Happy Birthday #{k}! You are now #{v} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end
